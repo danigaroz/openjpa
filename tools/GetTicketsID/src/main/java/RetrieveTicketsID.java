e9d2fffb2ac98c671ac714c926bb67eadd52c897
danigaroz <danigv1605@gmail.com>
2026-05-03 08:47:41 +0000

    Extend RetrieveTicketsID to save tickets metadata to CSV (key, created, resolutionDate, AV)

--- tools/GetTicketsID/src/main/java/RetrieveTicketsID.java
+++ tools/GetTicketsID/src/main/java/RetrieveTicketsID.java
@@ -8,28 +8,13 @@ import java.nio.charset.Charset;
 
 class RetrieveTicketsID {
 
-
-
-
    private static String readAll(Reader rd) throws IOException {
-	      StringBuilder sb = new StringBuilder();
-	      int cp;
-	      while ((cp = rd.read()) != -1) {
-	         sb.append((char) cp);
-	      }
-	      return sb.toString();
-	   }
-
-   public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
-      InputStream is = new URL(url).openStream();
-      try {
-         BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-         String jsonText = readAll(rd);
-         JSONArray json = new JSONArray(jsonText);
-         return json;
-       } finally {
-         is.close();
-       }
+      StringBuilder sb = new StringBuilder();
+      int cp;
+      while ((cp = rd.read()) != -1) {
+         sb.append((char) cp);
+      }
+      return sb.toString();
    }
 
    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
@@ -39,20 +24,20 @@ class RetrieveTicketsID {
          String jsonText = readAll(rd);
          JSONObject json = new JSONObject(jsonText);
          return json;
-       } finally {
+      } finally {
          is.close();
-       }
+      }
    }
 
+   public static void main(String[] args) throws IOException, JSONException {
+      String projName = "OPENJPA";
+      Integer j = 0, i = 0, total = 1;
 
-  
-  	   public static void main(String[] args) throws IOException, JSONException {
-		   
-		   String projName ="OPENJPA";
-	   Integer j = 0, i = 0, total = 1;
-      //Get JSON API for closed bugs w/ AV in the project
+      String outName = projName + "TicketsInfo.csv";
+      FileWriter fileWriter = new FileWriter(outName);
+      fileWriter.append("Key,Created,ResolutionDate,AffectedVersions\n");
+
       do {
-         //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
          j = i + 1000;
          String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
                 + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
@@ -62,13 +47,36 @@ class RetrieveTicketsID {
          JSONArray issues = json.getJSONArray("issues");
          total = json.getInt("total");
          for (; i < total && i < j; i++) {
-            //Iterate through each bug
-            String key = issues.getJSONObject(i%1000).get("key").toString();
+            JSONObject issue = issues.getJSONObject(i % 1000);
+            String key = issue.get("key").toString();
+            JSONObject fields = issue.getJSONObject("fields");
+
+            String created = fields.has("created") && !fields.isNull("created")
+                ? fields.get("created").toString() : "";
+            String resolutionDate = fields.has("resolutiondate") && !fields.isNull("resolutiondate")
+                ? fields.get("resolutiondate").toString() : "";
+
+            StringBuilder versions = new StringBuilder();
+            if (fields.has("versions") && !fields.isNull("versions")) {
+               JSONArray versionsArray = fields.getJSONArray("versions");
+               for (int v = 0; v < versionsArray.length(); v++) {
+                  if (v > 0) versions.append(";");
+                  versions.append(versionsArray.getJSONObject(v).get("name").toString());
+               }
+            }
+
+            fileWriter.append(key).append(",")
+                      .append(created).append(",")
+                      .append(resolutionDate).append(",")
+                      .append(versions.toString()).append("\n");
+
             System.out.println(key);
-         }  
+         }
       } while (i < total);
+
+      fileWriter.flush();
+      fileWriter.close();
+      System.out.println("Done. Total tickets: " + total + ". CSV: " + outName);
       return;
    }
-
- 
 }
